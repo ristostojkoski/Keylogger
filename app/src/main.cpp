46d7d514ee4658843da6a791e2f240e1328295b8
Risto Stojkoski <risto.stojkoski@gmail.com>
2019-04-03 21:50:04 +0200

    Created main.cpp in app/src

---
+++ app/src/main.cpp
@@ -0,0 +1,14 @@
+// ./app/src/
+#include <windows.h>
+
+int main()
+{
+   MSG msg;
+
+   while(GetMessage(&msg, NULL, 0, 0)){
+       TranslateMessage(&msg);
+       DispatchMessage(&msg);
+   }
+
+   return 0;
+}
