3a020522ea933ca425a2f89dd70c3afed8f4d82c
Risto Stojkoski <risto.stojkoski@gmail.com>
2019-04-04 21:10:34 +0200

    Removed folders 'app' and 'library' and updated CMakeLists.txt and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 // ./app/src/
 #include <windows.h>
-#include "../../library/include/Base64.h"
-#include "../../library/include/Helper.h"
-#include "../../library/include/IO.h"
-#include "../../library/include/KeyConstrants.h"
+#include "../include/Base64.h"
+#include "../include/Helper.h"
+#include "../include/IO.h"
+#include "../include/KeyConstrants.h"
 
 int main()
 {
